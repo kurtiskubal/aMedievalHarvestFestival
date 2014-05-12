66ce73eb01109c625cbf958fdbe9e638fcbdb6e6
zev <zev@zev-PC>
2014-05-12 09:54:45 -0600

    added super inheritance

--- aMedievalHarvestFestival/src/byui/cit260/aMedievalHarvestFestival/model/FoodItem.java
+++ aMedievalHarvestFestival/src/byui/cit260/aMedievalHarvestFestival/model/FoodItem.java
@@ -6,6 +6,8 @@
 
 package byui.cit260.aMedievalHarvestFestival.model;
 
+import java.util.Objects;
+
 /**
  * @version Adam
  * @author edit: Kurt
@@ -25,6 +27,41 @@ public class FoodItem extends InventoryItem {
 
     public void setFillingFactor(Double fillingFactor) {
         this.fillingFactor = fillingFactor;
+    }
+
+    @Override
+    public String toString() {
+// this pulls down the super class data
+        return super.toString() + "FoodItem{" + "fillingFactor=" + fillingFactor + '}';
+// <--end
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + Objects.hashCode(this.fillingFactor);
+// this pulls down the super class data
+        hash = hash + super.hashCode();
+// <--end
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final FoodItem other = (FoodItem) obj;
+        if (!Objects.equals(this.fillingFactor, other.fillingFactor)) {
+            return false;
+        }
+// this pulls down the super class data
+        boolean isEqual = super.equals(obj);
+        return isEqual;
+// <--end
     }
 
     
