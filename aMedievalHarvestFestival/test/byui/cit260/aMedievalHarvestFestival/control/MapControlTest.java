66c41566156e782fdc32076bbf2ec49a76ad168d
zev <zev@zev-PC>
2014-05-20 09:56:45 -0600

    stuff

--- aMedievalHarvestFestival/test/byui/cit260/aMedievalHarvestFestival/control/MapControlTest.java
+++ aMedievalHarvestFestival/test/byui/cit260/aMedievalHarvestFestival/control/MapControlTest.java
@@ -6,16 +6,26 @@
 
 package byui.cit260.aMedievalHarvestFestival.control;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author Kurt
+ * @author zev
  */
 public class MapControlTest {
     
     public MapControlTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
     }
 
     /**
@@ -24,100 +34,14 @@ public class MapControlTest {
     @Test
     public void testCalcSqFootageOfRoom() {
         System.out.println("calcSqFootageOfRoom");
-        double roomWidth = 10.0;
-        double roomLength = 5.0;
+        double roomWidth = 0.0;
+        double roomLength = 0.0;
         MapControl instance = new MapControl();
-        double expResult = 50.0;
+        double expResult = 0.0;
         double result = instance.calcSqFootageOfRoom(roomWidth, roomLength);
         assertEquals(expResult, result, 0.0);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
-    
-    
-    
-    /**
-     * Test 2
-     */
-    
-        System.out.println("calcSqFootageOfRoom Test 2");
-        roomWidth = 10.0;
-        roomLength = -1.0;
-        
-        expResult = -1.0;
-        
-        result = instance.calcSqFootageOfRoom(roomWidth, roomLength);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-         
-        /**
-     * Test 3
-     */
-    
-        System.out.println("calcSqFootageOfRoom Test 2");
-        roomWidth = -1.0;
-        roomLength = 5.0;
-        
-        expResult = -1.0;
-        
-        result = instance.calcSqFootageOfRoom(roomWidth, roomLength);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-         
-        /**
-     * Test 4
-     */
-    
-        System.out.println("calcSqFootageOfRoom Test 2");
-        roomWidth = 120.0;
-        roomLength = 50.0;
-        
-        expResult = -2.0;
-        
-        result = instance.calcSqFootageOfRoom(roomWidth, roomLength);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-         
-        /**
-     * Test 5
-     */
-    
-        System.out.println("calcSqFootageOfRoom Test 2");
-        roomWidth = 10.0;
-        roomLength = 0.0;
-        
-        expResult = 0.0;
-        
-        result = instance.calcSqFootageOfRoom(roomWidth, roomLength);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-         
-        /**
-     * Test 6
-     */
-    
-        System.out.println("calcSqFootageOfRoom Test 2");
-        roomWidth = 0.0;
-        roomLength = 32.0;
-        
-        expResult = 0.0;
-        
-        result = instance.calcSqFootageOfRoom(roomWidth, roomLength);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-         
-        /**
-     * Test 7
-     */
-    
-        System.out.println("calcSqFootageOfRoom Test 2");
-        roomWidth = 100.0;
-        roomLength = 50.0;
-        
-        expResult = 5000.0;
-        
-        result = instance.calcSqFootageOfRoom(roomWidth, roomLength);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-         
     }
+    
 }
