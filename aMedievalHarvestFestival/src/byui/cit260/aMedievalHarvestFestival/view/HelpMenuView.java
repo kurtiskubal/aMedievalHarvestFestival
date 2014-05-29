2031718162b352e04cf69328fc80be281ab7be69
Kurt <Kurt@permat4rd>
2014-05-29 16:23:37 -0600

    Views added

--- aMedievalHarvestFestival/src/byui/cit260/aMedievalHarvestFestival/view/HelpMenuView.java
+++ aMedievalHarvestFestival/src/byui/cit260/aMedievalHarvestFestival/view/HelpMenuView.java
@@ -72,28 +72,65 @@ public class HelpMenuView {
     public void doAction(char selection) {
         switch (selection) {
             case 'G':
-                System.out.println("G was selected");
+                System.out.println("\n\nIn order to beat the game, you must find evidence"
+                                  +"\nwhich can be found in and outside of the castle "
+                                  +"\nby examining items and objects that could have   "
+                                  +"\nplayed a role in the disappearance of the princess."
+                                  +"\nThis process is refered to in the game as gathering"
+                                  +"\nevidence. Evidence can also be gathered from talking"
+                                  +"\nto other characters that know important information.");
                 break;
             case 'M':
-                System.out.println("M was selected");
+                System.out.println("\n\nIn the Game Menu, you will have the option,"
+                                  +"\nby pressing 'M', to move to a new location.  "
+                                  +"\nWhen this option is selected, you will have the option"
+                                  +"\nof going directly to a previously visited location or"
+                                  +"\nyou will be able to view a map that show different room"
+                                  +"\nlocations.");
                 break;
             case 'T':
-                System.out.println("T was selected");
+                System.out.println("\n\nTo talk to other characters you will have to"
+                                  +"\nselect the option 'P' in the Game Menu. This will"
+                                  +"\nbring you to the Dialogue Menu, giving you several"
+                                  +"\nof who to talk to based on who is in the room. From"
+                                  +"\nthis general Dialogue Menu you will have the option"
+                                  +"\nto return to the Game Menu. Once in the Dialogue Menu,"
+                                  +"\nyou can select a specific person to talk to based on the"
+                                  +"\nnumber displayed next to their name in the Dialogue Menu.");
                 break;
             case 'F':
-                System.out.println("F was selected");
+                System.out.println("\n\nTo use food or beverage items you have to select"
+                                  +"\nthe 'U' Use Item option in the Game Menu. If an item"
+                                  +"\nis edible, you will have the option from there to consume"
+                                  +"\nit. To view how hungry your character is you have to select"
+                                  +"\nthe 'H' option in the Game Menu. To view how thirsty your"
+                                  +"\ncharacter is you have to select the 'T' option in the Game"
+                                  +"\nmenu.");
                 break;
             case 'E':
-                System.out.println("E was selected");
+                System.out.println("\n\nYour inventory, which contains all the items your"
+                                  +"\nplayer can carry, is able to be viewed from the Game"
+                                  +"\nMenu by selecting the 'V' option.");
                 break;
             case 'I':
-                System.out.println("I was selected");
+                System.out.println("\n\nTo get items you will first have to find them in"
+                                  +"\nthe various rooms and locations they can be found in."
+                                  +"\nOnce you are in a location, you can use the 'Examine Location'"
+                                  +"\noption found in the Game Menu to see the items in the location."
+                                  +"\nYou will then be able to examine items and pick them up by using"
+                                  +"\nthe 'EI' command in the Game Menu. This will give you a description"
+                                  +"\nof the item also with the options to put the item into the inventory,"
+                                  +"\nif it can be picked up, or to do nothing with the item.");
                 break;
             case 'L':
-                System.out.println("L was selected");
+                System.out.println("\n\nTo determine your location in the game"
+                                  +"\nyou can use the 'Examine Location' command"
+                                  +"\n(E) to find out where you currently are.");
                 break;
             case 'S':
-                System.out.println("S was selected");
+                System.out.println("\n\nYou can either save your progress at the Main"
+                                  +"\nMenu by selecting 'S' or you can select 'S' in "
+                                  +"\nthe Game Menu.");
                 break;
             case 'Q':
                 return;
